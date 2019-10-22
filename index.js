32273ad97de3f58fab20fa9ff4c88cfc9401d899
memesoundboard <50186369+absolute-music@users.noreply.github.com>
2019-10-22 14:56:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+let { message, Client } = require("discord.js");
+let client = new Discord.Client();
+var dbjs=function(token, game, message) {
+    if(token==="") {
+         throw new Error("Please input a token");
+    }
+    if(game){
+     client.setGame(game.length)
+    }
+   if(message){
+     message.channel.get(message.channel.id).send(message.send)
+   }
+   client.login(token).catch(throw new Error)
+}
+exports.dbjs=dbjs;
