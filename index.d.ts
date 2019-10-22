12d2190b5cbaaa33413feb8a27d70a4dad5aa29a
memesoundboard <50186369+absolute-music@users.noreply.github.com>
2019-10-22 14:32:02 -0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
+declare module "dbjs" {
+    export default function dbjs(input: string, options?: HasteBinOptions): Promise<string>;
 
+    export interface dbjsOptions {
+        token?: string;
+        extension?: string;
+    };
+}
